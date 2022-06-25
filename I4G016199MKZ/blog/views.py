da160a4de070f1d4d5d044a277fe90ef0a823f5c
wan-above <muhammedridwan02@gmail.com>
2022-06-25 07:24:10 +0100

    Finished the task

---
+++ I4G016199MKZ/blog/views.py
@@ -0,0 +1,37 @@
+
+from django.urls import reverse_lazy
+from .models import Post
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    DeleteView,
+    UpdateView,
+)
+
+
+# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
